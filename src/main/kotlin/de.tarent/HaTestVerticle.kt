ed7f7a851140d58295339a33029625fa3ac03f6a
Daniel Zerlett <daniel@zerlett.eu>
2018-09-12 16:15:45 +0200

    HA-test tool fixes

--- src/main/kotlin/de.tarent/HaTestVerticle.kt
+++ src/main/kotlin/de.tarent/HaTestVerticle.kt
@@ -12,14 +12,6 @@ class HaTestVerticle : AbstractVerticle() {
         vertx.setPeriodic(2000) { _ ->
             LOG.warning("Hello from HA verticle")
         }
-        val server = vertx.createHttpServer()
-        server.requestHandler { request ->
-            val response = request.response()
-            response.putHeader("content-type", "text/plain")
-            response.end("Hello World!")
-        }
-        server.listen(8080)
-        LOG.warning("HA verticle ending")
     }
 
 }
--- src/main/kotlin/de.tarent/KotlinVertxLab.kt
+++ src/main/kotlin/de.tarent/KotlinVertxLab.kt
@@ -1,6 +1,5 @@
 package de.tarent
 
-import com.hazelcast.config.Config
 import io.vertx.core.DeploymentOptions
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
@@ -8,22 +7,16 @@ import io.vertx.ext.shell.ShellService
 import io.vertx.ext.shell.ShellServiceOptions
 import io.vertx.ext.shell.term.TelnetTermOptions
 import java.util.logging.Logger
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager
-import io.vertx.core.spi.cluster.ClusterManager
 
 fun main(args: Array<String>) {
 
     val LOG = Logger.getLogger("main")
-    val startVerticle = (System.getenv("START_VERTICLE").compareTo("true") == 0)
+    val startVerticle = (System.getenv("START_VERTICLE") != null && System.getenv("START_VERTICLE").compareTo("true") == 0)
 
     LOG.info("starting kotlin vert.x lab")
 
-
     val options = VertxOptions()
-
-//    options.isHAEnabled = true
-//    options.isClustered = true
-
+    options.isHAEnabled = true
     Vertx.clusteredVertx(options) { cluster ->
         if (cluster.succeeded()) {
             val vertx = cluster.result()
